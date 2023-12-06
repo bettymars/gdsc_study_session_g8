ae0179bab1adbaef1ece849f8c8d8dcf22901aaa
betelhem <betelhemm1914@gmail.com>
2023-12-06 11:00:16 +0300

    Update task3.dart

--- Dart-Tasks/task3.dart
+++ Dart-Tasks/task3.dart
@@ -2,37 +2,41 @@ import 'dart:async';
 
 class Calculator {
   double add(double num1, double num2) {
-    return num1 + num2;
+    return (num1 + num2);
   }
 
   double subtract(double num1, double num2) {
-    return num1 - num2;
+    return (num1 - num2);
   }
 
   double multiply(double num1, double num2) {
-    return num1 * num2;
+    return (num1 * num2);
   }
 
   double divide(double num1, double num2) {
     if (num2 == 0) {
-      throw ZeroDivisionError('Cannot divide by zero');
+      throw ZeroDivisionError('Cannot divide by zero.');
     }
-    return num1 / num2;
+    return (num1 / num2);
   }
 }
 
 void main() async {
-  var calc = Calculator();
+  Calculator calculator = Calculator();
+
+  var num1, num2;
+  print('Enter first number: ');
+  num1 = double.parse(stdin.readLineSync()!);
+  print('Enter second number: ');
+  num2 = double.parse(stdin.readLineSync()!);
+
   double result;
-
   try {
-    result = calc.divide(10, 2); 
-  } catch (e) {
-    print('Error: $e');
-    return;
+    result = calculator.divide(num1, num2);
+    // Wait for 5 seconds
+    await Future.delayed(const Duration(seconds: 5));
+    print('Result after 5-second delay: $result');
+  } on ZeroDivisionError catch (e) {
+    print('Error: ${e.toString()}');
   }
-
-  await Future.delayed(Duration(seconds: 5), () {
-    print('Result: $result');
-  });
 }
